d32ce6cfd7473c95d3fa9f705a60da751500500d
Lucas Wanink <ratelsnake707@outlook.com>
2023-12-11 16:10:00 +0100

    Schets van inlogpagina gemaakt

--- index.php
+++ index.php
@@ -2,10 +2,42 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hallo Wereld!</title>
+    <title>Gemorskos</title>
 </head>
 <body>
+    <header class="header">
+        <div>
+            <img class="foto" src="img/Gemorskos logo zwart.png" alt="Logo">
+        </div>
+    </header>
+    <div class="signin_page">
+        <h1>Welkom, Log hier in!</h1>
+    </div>
     
+    <div class="login">
+        <form action="action_page.php" method="post">
+            <div class="login_img">
+                <img class="login_img2" src="img/Gemorskos logo wit.png" alt="Logo">
+            </div>
+        <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+        <div class="container">
+            <button class="cancelbtn" type="button">Cancel</button>
+            <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+        </form>
+    </div>
 </body>
 </html>
