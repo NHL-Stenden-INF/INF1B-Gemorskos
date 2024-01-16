c0aac873753960bd2294cb6397488f65b26aef6d
mart-Velema <mart.velema@student.nhlstenden.com>
2024-01-16 11:12:09 +0100

    Changed the mySQL connection to use PDO instead

--- db_userconnection.php
+++ db_userconnection.php
@@ -1,13 +1,16 @@
 <?php
-function OpenCon(){
     $dbhost = "127.0.0.1";
     $dbuser = "root";
     $dbpass = "1234";
     $dbname = "gemorskos_users";
-    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die("Connection Failed: %s\n". $conn -> error);
-    return $conn;
-}
-function CloseCon($conn){
-    $conn -> close();
-}
+    try
+    {
+        $dbHandler = new PDO ("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+        $log = 'Connected to db';
+    }
+    catch(PDOException $ex)
+    {
+        echo $ex;
+        $log = 'failed to connect';
+    };
 ?>
