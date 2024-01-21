86dfb073ec37eb81b3ce802a90223ca78c44e161
Lucas Wanink <ratelsnake707@outlook.com>
2024-01-21 16:23:16 +0100

    register php afgemaakt

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once "db_userconnection.php";
 require_once "session.php";
 
@@ -17,8 +18,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     $query->store_result();
         if ($query->num_rows > 0) {
             $error .= '<p>The email address is already registerd!</p>';
+        } else {
+            if (strlen($password) < 6) {
+                $error.= "<p>Password must have atleast 6 charactesr.</p>";
+            }
+
+            if (empty($confirm_password)) {
+                $error.= "<p>Please enter confirm password.</p>";
+            } else {
+                if (empty($error) && ($password != $confirm_password)) {
+                    $error .= "<p>Password dit not match.</p>"
+                }
+            }
+            if (empty($error)) {
+                $insertQuery = $db->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?);");
+                $insertQuery->bind_param("sss", $fullname, $email, $password_hash);
+                $result = $insertQuery->execute();
+                if ($result) {
+                    $error .= "<p>Your registration was successful!</p>";
+                } else {
+                    $error .= "<p>Something went wrong!</p>"
+                }
+            }
         }
     }
+    $query->close();
+    $insertQuery->close();
+    #mysqli_close($db) Niet voor ons, ivm PDO
 }
 
 ?>
